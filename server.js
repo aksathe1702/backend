e974a53130a3746180f7b66f73c9733e58e1dc9c
aksathe1702 <146939601+aksathe1702@users.noreply.github.com>
2025-05-01 13:10:33 +0530

    server

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ import createAdmin from './createAdmin.js'; // ✅ Import
 dotenv.config();
 
 const app = express();
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 const PORT = process.env.PORT || 5000;
 
 // Middleware
